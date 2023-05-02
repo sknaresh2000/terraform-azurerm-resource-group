c93bd186efe8c04eeb60ec7a336aa93b97e26f69
NareshKumaran Sathiaseelan <nareshs@NareshKumarans-MacBook-Air.local>
2023-05-02 17:16:30 -0400

    Added examples

---
+++ example/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">=1.2.8"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.47.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "=2.36.0"
+    }
+  }
+  backend "local" {}
+}
+
+provider "azurerm" {
+  features {}
+}
